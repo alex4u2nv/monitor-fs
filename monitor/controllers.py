65bb876c34b7b8ff76de44caff2efd46cfc1cfcd
alex <alex.mahabir@gmail.com>
2021-01-05 21:30:14 -0500

    Support for monitoring multiple folders

--- monitor/controllers.py
+++ monitor/controllers.py
@@ -1,6 +1,33 @@
+import argparse
+import os
+
+import yaml
 from watchdog.events import FileSystemEventHandler
 
 from monitor.services import UpdateQueue
+
+
+class ConfigHandler:
+    folders = []
+    queue_url = ""
+
+    def __init__(self, args: argparse.Namespace):
+        if args.folder and args.queue_url:
+            self.folders = args.folder.split(",")
+            self.queue_url = args.queue_url
+        elif args.config_file:
+            if not os.path.exists(args.config_file):
+                raise Exception("Config File {file} does not exist!".format(file=args.config_file))
+            with open(args.config_file, "r") as yml_file:
+                config = yaml.load(yml_file.read(), Loader=yaml.FullLoader)
+                self.folders = config['folders']
+                self.queue_url = config['queue_url']
+        self.__verify_folder_paths()
+
+    def __verify_folder_paths(self):
+        for folder in self.folders:
+            if not os.path.exists(folder):
+                raise Exception("Path {folder} to monitor doesn't exist".format(folder=folder))
 
 
 class EventsHandler(FileSystemEventHandler):
